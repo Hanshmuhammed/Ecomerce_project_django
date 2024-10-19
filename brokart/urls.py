022e7df9abcfe5b63c7457a7847c8da659f6fa6e
Hansh Muhammed <hanshmhd@gmail.com>
2024-10-19 14:04:03 +0530

    layout_design and product model (adding product to web over .till part 23)

--- brokart/urls.py
+++ brokart/urls.py
@@ -21,6 +21,8 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('products.urls'))
+    path('products/', include('products.urls')),
+    path('customrts/', include('customers.urls')),
+    path('order/', include('orders.urls'))
 ]
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
---
+++ customers/urls.py
@@ -0,0 +1,10 @@
+
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+from . import views
+
+urlpatterns = [
+    path('accout_page/', views.show_account, name='show_account'),
+]
